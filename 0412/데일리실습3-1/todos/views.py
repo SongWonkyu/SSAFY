266bea4347f4f41d831f644441a18072b90a466e
SongWonkyu <123xxx@kakao.com>
2023-04-12 17:36:33 +0900

    first commit

---
+++ 0412/데일리실습3-1/todos/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+@login_required
+def index(request):
+    todos = Todo.objects.all()
+    if not todos:
+        message = '작성된 글이 없습니다.'
+    else:
+        message = None
+    context={
+        'todos':todos,
+        'message': message,
+
+    }
+    return render(request, 'todos/index.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST, request.FILES)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.author = request.user
+            if form.cleaned_data['completed']:
+                todo.completed = True
+            else:
+                todo.completed = False
+            todo.save()
+            return redirect('todos:index')
+    else:
+        form = TodoForm()
+    context={
+        'form':form
+    }
+    return render(request, 'todos/create.html', context)
+
+
+def delete(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    if request.user == todo.author:
+        todo.delete()
+        return redirect('todos:index')
+
+def toggle(request, user_pk):
+    if request.user.is_authenticated:
+        todo = get_object_or_404(Todo, pk=user_pk)
+        todo.completed = not todo.completed  # Toggle the value
+        todo.save()
